cc4e50eea764ae7ea71e33695c38218b9aeb27c2
piverifiedads <piverifiedads@gmail.com>
2026-03-21 10:36:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 export default {
-  async fetch(request, env, ctx) {
+  async fetch(request, env) {
     const url = new URL(request.url);
 
-    // التحقق إذا كان الطلب لملف التحقق
+    // هذا الكود هو "الجسر" الذي سيفتح ملف رقم 8
     if (url.pathname === "/validation-key.txt") {
       return new Response("59a15951f520073db579a1427aa421e738f893ca33f984db3b3b67dd26d68886ccc3c6f7c93da2c24a08fc412b135ed379a2d5770b7d206fc97786be651dd532", {
-        headers: {
-          "content-type": "text/plain;charset=UTF-8",
-          "Access-Control-Allow-Origin": "*",
-        },
+        headers: { "content-type": "text/plain" },
       });
     }
 
-    // السماح لبقية الملفات (مثل index.html) بالظهور بشكل طبيعي
+    // لتشغيل باقي ملفات الموقع
     return env.ASSETS.fetch(request);
-  },
+  }
 };

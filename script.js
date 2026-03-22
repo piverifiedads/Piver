09fc7aa8dacc157f47954894f6c0f56d95483b7c
piverifiedads <piverifiedads@gmail.com>
2026-03-22 06:57:32 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function handleBuy() {
+    alert("سيتم توجيهك الآن لمحفظة Pi لإتمام عملية الشراء...");
+    // هنا مستقبلاً نضع كود Pi SDK الخاص بالدفع
+}
+
+function contactSeller(name) {
+    alert("فتح الدردشة مع البائع: " + name);
+}
